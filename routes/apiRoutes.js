ab9f83687b70aa162265b58885eda4548b17f950
Jarvis Maness <you@example.com>
2019-08-03 12:36:46 -0400

    pulling data from api for type of beer

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,76 +2,57 @@ var db = require("../models");
 
 module.exports = function (app) {
   // Get all examples
-<<<<<<< HEAD
   app.get("/api/beers", function(req, res) {
     db.Beer.findAll({}).then(function(dbBeer) {
       console.log("Name of Beer!")
-=======
-  app.get("/api/beers", function (req, res) {
-    db.Beer.findAll({}).then(function (dbBeer) {
->>>>>>> fbdabac2cac6a1be6327315c4fb22c95a9a7b74c
       res.json(dbBeer);
     });
   });
 
   //Get route for returning post of the specific type of beer//
 
-  app.get("/api/beer/type/:type", function (req, res) {
+  app.get("/api/type/:type", function (req, res) {
 
     db.Beer.findAll({
       where: {
         type: req.params.type
       }
     }).then(function (dbBeer) {
+      console.log("Type of Beer!");
       res.json(dbBeer);
     })
   })
 
-  .then(function(dbBeer){
-  console.log("beer type is here");
-    res.json(dbBeer);
-  })
+  
 
 
   //Get route to searching by Brewery Name//
-  app.get("/api/beers/breweryName/:breweryName", function (req, res) {
+  app.get("/api/breweryname/:breweryname", function (req, res) {
     db.Beer.findAll({
       where: {
-        breweryName: req.params.breweryName
+        breweryName: req.params.breweryname
       }
     }).then(function (dbBeer) {
+      console.log("Name of Brewery is here!");
       res.json(dbBeer);
     })
   })
-<<<<<<< HEAD
-  .then(function (dbBeer) {
-    console.log("Brewery Name is here")
-    res.json(dbBeer);
-  })
-=======
->>>>>>> fbdabac2cac6a1be6327315c4fb22c95a9a7b74c
-
+  
 
 
 
   //Get route for returning post of the specific type of beer//
-  app.get("/api/beers/abv/:abv", function (req, res) {
+  app.get("/api/abv/:abv", function (req, res) {
     db.Beer.findAll({
       where: {
         abv: req.params.abv
       }
     }).then(function (dbBeer) {
+      console.log("Percentage of beer is here!");
       res.json(dbBeer);
     })
   })
-<<<<<<< HEAD
-  .then(function (dbBeer) {
-    console.log("Percentage is here")
-    res.json(dbBeer);
-  })
-=======
-
->>>>>>> fbdabac2cac6a1be6327315c4fb22c95a9a7b74c
+  
 
   // Create a new beer
   //app.post("/api/addbeer", function(req, res) {
