3c714ef04a721ff0171bdc1116b4f3014e8dcf5c
Nick O <nicholas.oetinger@gmail.com>
2019-08-10 01:21:47 -0400

    fucking work

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -19,15 +19,6 @@ module.exports = function (app) {
     })
   });
 
-  // // Load example page and pass in an example by id
-  // app.get("/beer/:type", function(req, res) {
-  //   db.Beer.findOne({ where: { type: req.params.type } }).then(function(beerList) {
-  //     res.render("index", {
-  //       beers: beerList
-  //     });
-  //   });
-  // });
-
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
     res.render("404");
